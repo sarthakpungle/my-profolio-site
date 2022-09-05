e07cca3dc927a2c5b7f1e9c596158d78b2e797c7
sarthakpungle <111989180+sarthakpungle@users.noreply.github.com>
2022-09-05 16:33:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 function changeImageOver() 
 {
 document.getElementById("profile").src="my.jpg";
-document.getElementById("profile").style.height="500px";
 
 }
 
@@ -22,15 +21,3 @@ function ko(){
     hi.style.border="0px"; 
     document.getElementById("hi").innerHTML="Hi!";
 }
-
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-    document.body.style.backgroundColor = "white";
-  }
